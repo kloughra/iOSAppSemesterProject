9e3e6ae0b2ceff426851c66f7ced07f1c79c44df
kloughra <katie.loughran@gmail.com>
2016-03-29 16:42:07 -0400

    Intial Project Commit

---
+++ NDRugby/NDRugby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NDRugby
+//
+//  Created by Katie Loughran on 3/29/16.
+//  Copyright © 2016 Katie Loughran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
