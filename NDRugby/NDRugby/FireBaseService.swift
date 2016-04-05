caeac261e8cb4861ddc6c99a4ac6a03be1474084
kloughra <katie.loughran@gmail.com>
2016-04-05 15:39:37 -0400

    Removed Podfiles after running into issues Installing Firebase

---
+++ NDRugby/NDRugby/FireBaseService.swift
@@ -0,0 +1,11 @@
+//
+//  FireBaseService.swift
+//  NDRugby
+//
+//  Created by Katie Loughran on 4/5/16.
+//  Copyright © 2016 Katie Loughran. All rights reserved.
+//
+
+import Foundation
+//import Firebase
+
